0d61a1ae3401888bd71844d5a80a915c9b8203c9
raaghulrdeveloper@gmail.com <r284619735@>
2017-03-30 01:19:54 +0530

    Logic Implemented

--- app/src/main/java/com/raaghulr/rr/squarerootcalculatorpro/MainActivity.java
+++ app/src/main/java/com/raaghulr/rr/squarerootcalculatorpro/MainActivity.java
@@ -1,13 +1,35 @@
 package com.raaghulr.rr.squarerootcalculatorpro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et_getvalue;
+    TextView tv_resultvalue;
+    double var_store,result_store;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //intialise
+        et_getvalue = (EditText)findViewById(R.id.et_getvalue);
+        tv_resultvalue = (TextView)findViewById(R.id.tv_resultvalue);
+        //
     }
+
+    public void squareCalculator (View view)
+    {
+        var_store = new Double(String.valueOf(et_getvalue.getText()));
+        result_store = Math.sqrt(var_store);
+        tv_resultvalue.setText((new String(String.valueOf(result_store))));
+        Toast.makeText(this, new String("Square Root Value For \nNumber : "+var_store+"  is "+String.valueOf(result_store)), Toast.LENGTH_LONG).show();
+    }
+
+
 }
